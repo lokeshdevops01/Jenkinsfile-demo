2fb7486b7f792e653843fa14078360c323f11754
lokeshdevops01 <mankarsl28@gmail.com>
2026-04-29 12:29:36 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,13 @@
 package com.example;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
 
     @Test
     public void testMain() {
         App.main(new String[]{});
+        assertTrue(true);
     }
 }
